7136414cb2f5d027fe95e4859259da9613c9c000
jisboui <jihedsbouii@gmail.com>
2024-02-10 19:40:02 +0100

    title change

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -4,5 +4,13 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: 'http://localhost:8000'
+  },
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'زيت السبوعي';
+        return args;
+      })
   }
 })
